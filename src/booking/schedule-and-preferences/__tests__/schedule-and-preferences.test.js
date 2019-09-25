c7287ec6b7392a41acbb3303670068b12e69a8a1
Lilei Huang <lilei@carbonfive.com>
2019-09-25 15:03:27 -0700

    Tracker#168207852 handle errors from bookAppointment

--- src/booking/schedule-and-preferences/__tests__/schedule-and-preferences.test.js
+++ src/booking/schedule-and-preferences/__tests__/schedule-and-preferences.test.js
@@ -13,6 +13,7 @@ jest.mock('images/noun_hairstyle_1105146.svg', () => {})
 jest.mock('images/noun_shampoo_2677530.svg', () => {})
 jest.mock('vue-loading-overlay/dist/vue-loading.css', () => {})
 jest.mock('images/noun_payment_511229.svg', () => {})
+jest.mock('../../images/group-5.svg', () => {})
 
 describe('ScheduleAndPreferences', () => {
   let wrapper
@@ -88,8 +89,78 @@ describe('ScheduleAndPreferences', () => {
   })
 
   describe('#bookAppointment', () => {
-    it('shows page is loading while awaiting for async requests', () => {})
-    it('when there are errors, page is done loading', () => {})
-    it('when there are errors, shows an error message', () => {})
+    let vm
+    let $router
+
+    beforeEach(() => {
+      $router = {
+        push: jest.fn()
+      }
+
+      Storage.sharedState.product = {
+        name: 'A fake product'
+      }
+
+      vm = wrapper = shallowMount(ScheduleAndPreferences, {
+        mocks: { $router }
+      }).vm
+    })
+
+    it('shows page in a loading state', () => {
+      expect(vm.isLoading).toEqual(false)
+      vm.bookAppointment()
+      expect(vm.isLoading).toEqual(true)
+    })
+
+    it('page stops loading when all requests are successful', async () => {
+      expect(vm.isLoading).toEqual(false)
+
+      vm.createOrder = jest.fn().mockResolvedValue({})
+      vm.addStyleToCart = jest.fn().mockResolvedValue({})
+      vm.addAddOnsToCart = jest.fn().mockResolvedValue({})
+      vm.createOrUpdateUser = jest.fn().mockResolvedValue({})
+      vm.completeCheckout = jest.fn().mockResolvedValue({})
+
+      await vm.bookAppointment()
+      expect(vm.isLoading).toEqual(false);
+    })
+
+    it('routes to confirmation page when all requests are successful', async () => {
+      vm.createOrder = jest.fn().mockResolvedValue({})
+      vm.addStyleToCart = jest.fn().mockResolvedValue({})
+      vm.addAddOnsToCart = jest.fn().mockResolvedValue({})
+      vm.createOrUpdateUser = jest.fn().mockResolvedValue({})
+      vm.completeCheckout = jest.fn().mockResolvedValue({})
+
+      await vm.bookAppointment()
+      expect($router.push).toHaveBeenCalledWith({name: 'confirmation'})
+    })
+
+    it('when any of the requests errors, page stops loading', async () => {
+      vm.createOrder = jest.fn().mockResolvedValue({})
+      vm.addStyleToCart = jest.fn().mockRejectedValue({})
+
+      await vm.bookAppointment()
+      expect(vm.isLoading).toEqual(false);
+    })
+
+    it('when any of the requests errors, submitting again will only call the failed request again', async () => {
+      vm.createOrder = jest.fn().mockResolvedValue({})
+      vm.addStyleToCart = jest.fn()
+        .mockRejectedValueOnce({})
+        .mockResolvedValueOnce({})
+      vm.addAddOnsToCart = jest.fn().mockResolvedValue({})
+      vm.createOrUpdateUser = jest.fn().mockResolvedValue({})
+      vm.completeCheckout = jest.fn().mockResolvedValue({})
+
+      await vm.bookAppointment()
+      await vm.bookAppointment()
+
+      expect(vm.createOrder).toHaveBeenCalledTimes(1)
+      expect(vm.addStyleToCart).toHaveBeenCalledTimes(2)
+      expect(vm.addAddOnsToCart).toHaveBeenCalledTimes(1)
+      expect(vm.createOrUpdateUser).toHaveBeenCalledTimes(1)
+      expect(vm.completeCheckout).toHaveBeenCalledTimes(1)
+    })
   })
 })
